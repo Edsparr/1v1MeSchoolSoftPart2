f07032ec0d5a319e011f3a311ff061504ad4b432
Surda <surda.kontakt@gmail.com>
2019-12-19 21:33:30 +0100

    Comments

--- SchoolClassTester/CommandListener.cs
+++ SchoolClassTester/CommandListener.cs
@@ -20,20 +20,21 @@ namespace SchoolClassTester
         {
             if (Container != null)
                 throw new Exception(nameof(Container));
+
             var builder = new ContainerBuilder();
             builder.RegisterType<FileService>().InstancePerLifetimeScope()
-                .As<IFileService>();
+                .As<IFileService>(); //Registers file service as IFileService
 
             var commands = typeof(ICommand).Assembly
                 .GetTypes()
-                .Where(c => !c.IsAbstract && c.GetInterfaces().Contains(typeof(ICommand)));
+                .Where(c => !c.IsAbstract && c.GetInterfaces().Contains(typeof(ICommand))); //Finds all classes in this assembly (project) that inherits from ICommand and isn't abstract
             foreach(var command in commands)
             {
-                builder.RegisterType(command).As<ICommand>();
-                builder.RegisterDecorator(command, typeof(ICommand));
+                builder.RegisterType(command).As<ICommand>(); //Register the command type as ICommand
+                builder.RegisterDecorator(command, typeof(ICommand)); //Autofac thingy, makes it so we can resolve ICommand as a collection, aka more than 1 command ata time
             }
 
-            Container = builder.Build();
+            Container = builder.Build(); //I build the container to show it's finished being configured
         }
 
 
@@ -41,22 +42,23 @@ namespace SchoolClassTester
         {
             while (true)
             {
-                string rawInput = Console.ReadLine();
-                var splitted = rawInput.Split(' ');
-                var command = splitted.First();
+                string rawInput = Console.ReadLine(); 
+                var splitted = rawInput.Split(' '); //Splitting for parsing
+                var command = splitted.First(); //Command name
                 string args = "";
                 if(rawInput.Length > command.Length + 1)
-                    args = rawInput.Remove(0, splitted.First().Length + 1); //Command length + the space
+                    args = rawInput.Remove(0, splitted.First().Length + 1); //Command length + the space means we got args after
 
-                var foundCommands = Container.Resolve<IEnumerable<ICommand>>();
-                var foundCommand = foundCommands.SingleOrDefault(c => c.Name.Equals(command, StringComparison.InvariantCultureIgnoreCase));
-                if(foundCommand == null)
+                var foundCommands = Container.Resolve<IEnumerable<ICommand>>(); //I get all classes that are registred as ICommand which is all Commands
+                var foundCommand = foundCommands.SingleOrDefault(c => c.Name.Equals(command, StringComparison.InvariantCultureIgnoreCase)); 
+                //I get one command that has the same command name as the input command name
+                if(foundCommand == null) //If I can't find one I tell the user and try again
                 {
                     Console.WriteLine($"Command not found: {command}!");
                     continue;
                 }
-                var result = foundCommand.Execute(args);
-                Console.WriteLine(result);
+                var result = foundCommand.Execute(args); //I execute the command with args
+                Console.WriteLine(result); //I print the result it gave me via return value.
             }
         }
     }
